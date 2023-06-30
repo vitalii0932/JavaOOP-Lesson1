cfdab5f833cc3b2c2314db8699d5c194166adade
VitaliiGalayko <107875708+vitalii0932@users.noreply.github.com>
2023-06-30 03:33:12 +0300

    Create Triangle.java

---
+++ Lesson1/src/basicLevel/Triangle.java
@@ -0,0 +1,57 @@
+package basicLevel;
+
+public class Triangle {
+    private double sideA, sideB, sideC;
+
+    public Triangle(double sideA, double sideB, double sideC) {
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+    }
+
+    public Triangle() {
+    }
+
+    public double getSideA() {
+        return sideA;
+    }
+
+    public void setSideA(double sideA) {
+        this.sideA = sideA;
+    }
+
+    public double getSideB() {
+        return sideB;
+    }
+
+    public void setSideB(double sideB) {
+        this.sideB = sideB;
+    }
+
+    public double getSideC() {
+        return sideC;
+    }
+
+    public void setSideC(double sideC) {
+        this.sideC = sideC;
+    }
+
+    public void getArea() {
+        if(sideA > 0 && sideB > 0 && sideC > 0) {
+            if(sideA + sideB > sideC && sideA + sideC > sideB && sideB + sideC > sideA)
+            {
+                double p = sideA + sideB + sideC;
+                System.out.printf("Area of your triangle = %f\n", Math.sqrt(p * (p - sideA) * (p - sideB) * (p - sideC)));
+            } else {
+                System.out.println("Triangle don't exist.");
+            }
+        } else {
+            System.out.println("One side = 0");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "triangle{" + "sideA=" + sideA + ", sideB=" + sideB + ", sideC=" + sideC + '}';
+    }
+}
