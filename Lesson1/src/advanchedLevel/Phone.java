228ba95533f71d8eb22dd214e18a5c3c2905c90f
VitaliiGalayko <107875708+vitalii0932@users.noreply.github.com>
2023-06-30 23:12:59 +0300

    Update Phone.java

--- Lesson1/src/advanchedLevel/Phone.java
+++ Lesson1/src/advanchedLevel/Phone.java
@@ -2,10 +2,11 @@ package advanchedLevel;
 
 public class Phone {
     private String phoneNumber;
-    private String networkName;
+    private String userName;
 
-    public Phone(String phoneNumber) {
+    public Phone(String phoneNumber, String userName) {
         this.phoneNumber = phoneNumber;
+        this.userName = userName;
     }
 
     public Phone() {
@@ -19,48 +20,55 @@ public class Phone {
         this.phoneNumber = phoneNumber;
     }
 
-    public String getNetworkName() {
-        return networkName;
+    public String getUserName() {
+        return userName;
     }
 
-    public void setNetworkName(String networkName) {
-        this.networkName = networkName;
+    public void setUserName(String userName) {
+        this.userName = userName;
     }
 
     public void register(Network network) {
-        this.networkName = network.getName();
+        if(this.phoneNumber.length() == 7) { // +38(063)***-**-**; 063 -> networkID
+            this.phoneNumber = network.getID() + this.phoneNumber;
+            System.out.printf("Registration completed. %s, your phone number is %s\n", this.userName, this.phoneNumber);
+        }
+
+        else
+            System.out.println("Registration error.");
     }
 
-    public void call(Phone phone) {
-        if(phone.getPhoneNumber() != null && this.phoneNumber != null && this.phoneNumber.length() > 10 && phone.getPhoneNumber().length() > 10) {
-            if(this.networkName != null) {
-                if(this.phoneNumber != phone.getPhoneNumber()) {
-                    if(phone.getNetworkName() != null) {
-                        System.out.println("call... call... call..");
+    public void call(String callingPhoneNumber) {
+        if(this.phoneNumber != null && this.phoneNumber.length() == 10) { //does user phone exist
+            if(callingPhoneNumber != null && callingPhoneNumber.length() == 10) { //does called phone exist
+                if(this.phoneNumber.substring(0,3).equals("093") || this.phoneNumber.substring(0,3).equals("095") || this.phoneNumber.substring(0,3).equals("097")) {
+                    //does user phone registered
+                    if(this.phoneNumber != callingPhoneNumber) { //does user don't call himself
+                        if(callingPhoneNumber.substring(0,3).equals("093") || callingPhoneNumber.substring(0,3).equals("095") || callingPhoneNumber.substring(0,3).equals("097")) { //does called phone registered
+                            calls(callingPhoneNumber);
+                        } else {
+                            System.out.println("ERROR. Unknown network.");
+                        }
                     } else {
-                        System.out.println("The phone number of the person you are calling not registered.");
+                        System.out.println("You can't call yourself");
                     }
                 } else {
-                    System.out.println("You can't call yourself");
+                    System.out.println("Unknown network. Please, register your phone number");
                 }
             } else {
-                System.out.println("Please, register your phone number");
+                System.out.println("The phone number you are calling is not correct");
             }
         } else {
-            System.out.println("ERROR. Please input phone number");
+            System.out.println("Please, register your phone number, or input another phone number");
         }
     }
 
-    public void calls(Phone phone) {
-        if(phone.getPhoneNumber() != null && phone.getPhoneNumber().length() > 10){
-            System.out.printf("%s calls you\n", phone.getPhoneNumber());
-        } else {
-            System.out.println("ERROR");
-        }
+    public void calls(String calledPhoneNumber) {
+        System.out.printf("%s calling: call... call... call..\n", calledPhoneNumber);
     }
 
     @Override
     public String toString() {
-        return "Phone{" + "phoneNumber='" + phoneNumber + '\'' + ", networkName='" + networkName + '\'' + '}';
+        return "Phone{" + "phoneNumber='" + phoneNumber + '\'' + ", UserName='" + userName + '\'' + '}';
     }
 }
