70c535f9871c0fc854e90772c9d93796962f4944
VitaliiGalayko <107875708+vitalii0932@users.noreply.github.com>
2023-07-08 12:55:07 +0300

    Update Main.java

--- Lesson1/src/advanchedLevel/Main.java
+++ Lesson1/src/advanchedLevel/Main.java
@@ -2,18 +2,15 @@ package advanchedLevel;
 
 public class Main {
     public static void main(String[] args) {
-        Network network1 = new Network("Vodafone", "095");
-        Network network2 = new Network("KyivStar", "095");
-        Network network3 = new Network("Lifecell", "095");
+        Phone phone1 = new Phone("0936548675", "Danil");
+        Phone phone2 = new Phone("0936565345", "Anton");
+        Network network = new Network("Vodafon");
 
-        Phone phone1 = new Phone("7763498", "Vania");
-        Phone phone2 = new Phone("1765676", "Danila");
-
-        phone1.call(phone2.getPhoneNumber());
-        phone1.register(network1);
-        phone1.call(phone1.getPhoneNumber());
-        phone1.call(phone2.getPhoneNumber());
-        phone2.register(network2);
-        phone1.call(phone2.getPhoneNumber());
+        phone1.call("0936565345");
+        phone1.register();
+        phone1.call("0936565345");
+        phone2.register();
+        phone1.call("0936565345");
+        System.out.println(phone1.toString());
     }
 }
